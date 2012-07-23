b0fd9996de85a2552adb472066962fb9b02915a8
Robin Vierich <robinvierich@gmail.com>
2012-07-22 23:02:09 -0400

    fixed types in message classes

--- messages.py
+++ messages.py
@@ -56,12 +56,12 @@ class ConnectResponse(Message):
     
 class DisconnectRequest(Message):
     def __init__(self, check_for_unreplicated_files):
-        super(DisconnectRequest, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(DisconnectRequest, self).__init__(MessageType.DISCONNECT_REQUEST)
         self.check_for_unreplicated_files = check_for_unreplicated_files
     
 class DisconnectResponse(Message):
     def __init__(self, should_wait):
-        super(DisconnectResponse, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(DisconnectResponse, self).__init__(MessageType.DISCONNECT_RESPONSE)
         self.should_wait = should_wait
 
 
@@ -94,7 +94,7 @@ class FileData(Message):
 
 class FileChanged(Message):
     def __init__(self, file_path, new_checksum, new_data, start_offset):
-        super(FileChanged, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(FileChanged, self).__init__(MessageType.FILE_CHANGED)
         self.file_path = file_path
         self.new_checksum = new_checksum
         self.new_data = new_data
@@ -102,21 +102,21 @@ class FileChanged(Message):
 
 class ValidateChecksumRequest(Message):
     def __init__(self, file_path, file_checksum):
-        super(ValidateChecksumRequest, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(ValidateChecksumRequest, self).__init__(MessageType.VALIDATE_CHECKSUM_REQUEST)
         self.file_path = file_path    
         self.file_checksum = file_checksum
 
 # note, made a change to the doc we made here. Adding file_path
 class ValidateChecksumResponse(Message):
     def __init__(self, file_path, valid):
-        super(ValidateChecksumResponse, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(ValidateChecksumResponse, self).__init__(MessageType.VALIDATE_CHECKSUM_RESPONSE)
         self.file_path = file_path
         self.valid = valid
 
 
 class NewFileAvailable(Message):
     def __init__(self, file_path, file_checksum, file_data):
-        super(NewFileAvailable, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(NewFileAvailable, self).__init__(MessageType.NEW_FILE_AVAILABLE)
         self.file_path = file_path    
         self.file_checksum = file_checksum
         self.file_data = file_data
@@ -124,33 +124,33 @@ class NewFileAvailable(Message):
 # doc says (file_id), changed to (file_path)
 class DeleteRequest(Message):
     def __init__(self, file_path):
-        super(DeleteRequest, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(DeleteRequest, self).__init__(MessageType.DELETE_REQUEST)
         self.file_path = file_path
 
 # added (file_path) here
 class DeleteResponse(Message):
     def __init__(self, file_path, can_delete):
-        super(DeleteResponse, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(DeleteResponse, self).__init__(MessageType.DELETE_RESPONSE)
         self.file_path = file_path 
         self.can_delete = can_delete
 
 # doc says (file_id), changed to (file_path)
 class Delete(Message):
     def __init__(self, file_path):
-        super(Delete, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(Delete, self).__init__(MessageType.DELETE)
         self.file_path = file_path   
 
 # doc says (file_id, dest_path) changed to (source_path, dest_path)
 class MoveRequest(Message):
     def __init__(self, source_path, dest_path):
-        super(MoveRequest, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(MoveRequest, self).__init__(MessageType.MOVE_REQUEST)
         self.source_path = source_path
         self.dest_path = dest_path
         
 # doc says just (valid) changed to (source_path, dest_path, valid)
 class MoveResponse(Message):
     def __init__(self, source_path, dest_path, valid):
-        super(MoveResponse, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(MoveResponse, self).__init__(MessageType.MOVE_RESPONSE)
         self.source_path = source_path
         self.dest_path = dest_path
         self.valid = valid
@@ -158,14 +158,14 @@ class MoveResponse(Message):
 # doc says (file_id, dest_path) changed to (source_path, dest_path)  
 class Move(Message):
     def __init__(self, source_path, dest_path):
-        super(Move, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(Move, self).__init__(MessageType.MOVE)
         self.source_path = source_path
         self.dest_path = dest_path
 
 
 class ListRequest(Message):
     def __init__(self, dir_path=None):
-        super(ListRequest, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(ListRequest, self).__init__(MessageType.LIST_REQUEST)
         self.dir_path = dir_path
         
 
@@ -176,13 +176,13 @@ class List(Message):
         
 class ArchiveRequest(Message):
     def __init__(self, file_path):
-        super(ArchiveRequest, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(ArchiveRequest, self).__init__(MessageType.ARCHIVE_REQUEST)
         self.file_path = file_path
 
 # added file_path
 class ArchiveResponse(Message):
     def __init__(self, file_path, archived):
-        super(ArchiveResponse, self).__init__(MessageType.CONNECT_RESPONSE)
+        super(ArchiveResponse, self).__init__(MessageType.ARCHIVE_RESPONSE)
         self.file_path = file_path
         self.archived = archived
         
